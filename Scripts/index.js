e54d7ca3da88df3ad91f5d6e7ed29ba19d888b5d
Sami Hattab <hattab_sami@yahoo.fr>
2019-03-18 16:38:42 +0100

    Ajout du Leap Motion

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,8 +11,12 @@ const game = new Phaser.Game(
     }
 );
 
-let cube;
-let cube2
+const Pieces = [];
+let Tower_player_one = [];
+let Tower_player_two = [];
+// const positions = [30,90,150,210,270,330,390,450,510];
+
+let faisceau;
 
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -20,32 +24,68 @@ function preload() {
     // game.scale.pageAlignVertically = true;
     // game.stage.backgroundColor = '#eee';
 
-    game.load.image('cube', '../Images/cube.jpg');
-    game.load.image('cube2', '../Images/cube.jpg');
+    game.load.image('background', 'assets/Images/Background.jpg');
+
 }
 
 function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.startSystem(Phaser.Physics.Box2D);
 
-    cube = game.add.sprite(50, 50, 'cube');
-    cube2 = game.add.sprite(50, -2000, 'cube2');
-    cube.anchor.set(0.5);
-    cube2.anchor.set(0.5);
-    game.physics.arcade.enable(cube);
-    game.physics.arcade.enable(cube2);
+    background = game.add.image(game.world.centerX, game.world.centerY, 'background').anchor.set(0.5);
+
+    faisceau = new Phaser.Rectangle(510, 50, 50, 500);
     
-    cube.body.velocity.set(0, 300);
 }
 
 function update() {
-    if (cube.y > window.innerHeight) {
-        cube.body.velocity.set(0, 0);
-        cube.body.immovable=true;
-        cube2.body.velocity.set(0, 300);
-        game.physics.arcade.collide(cube, cube2)
+    if(LEAP.connected){
+        faisceau.x = LEAP.position.x
+        if(faisceau.x > 0 && faisceau.x < 60){
+            faisceau.x = 30;
+        } else if(faisceau.x > 60 && faisceau.x < 120){
+            faisceau.x = 90;
+        } else if(faisceau.x > 120 && faisceau.x < 180){
+            faisceau.x = 150;
+        } else if(faisceau.x > 180 && faisceau.x < 240){
+            faisceau.x = 210;
+        } else if(faisceau.x > 240 && faisceau.x < 300){
+            faisceau.x = 270;
+        } else if(faisceau.x > 300 && faisceau.x < 360){
+            faisceau.x = 330;
+        } else if(faisceau.x > 360 && faisceau.x < 420){
+            faisceau.x = 390;
+        } else if(faisceau.x > 420 && faisceau.x < 480){
+            faisceau.x = 450;
+        } else if(faisceau.x > 480 && faisceau.x < 540){
+            faisceau.x = 510;
+        } else if(faisceau.x > 540 && faisceau.x < 600){
+            faisceau.x = 570;
+        } else if(faisceau.x > 600 && faisceau.x < 660){
+            faisceau.x = 630;
+        } else if(faisceau.x > 660 && faisceau.x < 720){
+            faisceau.x = 690;
+        } else if(faisceau.x > 720 && faisceau.x < 780){
+            faisceau.x = 750;
+        } else if(faisceau.x > 780 && faisceau.x < 840){
+            faisceau.x = 810;
+        } else if(faisceau.x > 840 && faisceau.x < 900){
+            faisceau.x = 870;
+        } else if(faisceau.x > 900 && faisceau.x < 960){
+            faisceau.x = 930;
+        } else if(faisceau.x > 960 && faisceau.x < 1020){
+            faisceau.x = 990;
+        } else if(faisceau.x > 1020 && faisceau.x < 1080){
+            faisceau.x = 1050;
+        } else if(faisceau.x > 1080){
+            faisceau.x = 1110
+        }
+    } else {
+        faisceau.x = game.input.x || game.world.width*0.5;
     }
+
+    
 }
 
 function render() {
-
+    game.debug.geom(faisceau,'#0fffff');
 }
---
+++ Scripts/leapInput.js
@@ -0,0 +1,42 @@
+const LEAP = {
+    position : {
+        x : 0,
+        y : 0,
+    },
+    connected : false,
+};
+
+const controller = new Leap.Controller();
+
+controller.connect();
+
+controller.on('deviceStreaming', () => {
+    LEAP.connected = true;
+    console.log('✔ Leap is connected')
+});
+
+controller.on('deviceDisconnected', function() {
+    LEAP.connected = false;
+    console.log('❌ Leap disconnected');
+});
+
+controller.on('frame', function(frame) {
+    let hand = frame.hands[0];
+    if (!hand) return;
+
+    const palm = get2dCoords(hand.stabilizedPalmPosition, frame);
+    LEAP.position.x = palm.x;
+    LEAP.position.y = palm.y;
+
+    //console.log(LEAP.position)
+});
+
+function get2dCoords(leapPosition, frame) {
+    const interactionBox = frame.interactionBox;
+    const normalizedPoint = interactionBox.normalizePoint(leapPosition, true);
+
+    return {
+        x : normalizedPoint[0] * window.innerWidth,
+        y : (1 - normalizedPoint[1]) * window.innerHeight
+    };
+}
