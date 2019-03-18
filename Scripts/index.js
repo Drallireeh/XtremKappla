0908b0c4cca083794cb2be31ce43588fdbdb5231
kenny herillard <Herillard>
2019-03-18 16:26:11 +0100

    Load physics.json to have real collisions

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,8 +11,7 @@ const game = new Phaser.Game(
     }
 );
 
-let cube;
-let cubeGroup;
+let piece_one;
 let cube2
 
 function preload() {
@@ -20,32 +19,34 @@ function preload() {
     // game.scale.pageAlignHorizontally = true;
     // game.scale.pageAlignVertically = true;
     // game.stage.backgroundColor = '#eee';
+    game.load.image('piece_one', '../Assets/Images/piece_one.png');
+    game.load.image('cube2', '../Assets/Images/cube.png');
 
-    game.load.image('cube', '../Images/cube.png');
-    game.load.image('cube2', '../Images/cube.png');
+    game.load.physics('physicsData', 'Assets/physics.json');
 }
 
 function create() {
     game.physics.startSystem(Phaser.Physics.P2JS);
     let blockCollisionGroup = game.physics.p2.createCollisionGroup();
     game.physics.p2.updateBoundsCollisionGroup();
-    
-    cube = game.add.sprite(100, 100, 'cube');
-    game.physics.p2.enable(cube);
-    cube.body.setCollisionGroup(blockCollisionGroup);
-    cube.body.collides([blockCollisionGroup]);
+
+    piece_one = game.add.sprite(100, 100, 'piece_one');
+    game.physics.p2.enable(piece_one);
+    piece_one.body.clearShapes();
+    piece_one.body.loadPolygon('physicsData', 'piece_one');
+    piece_one.body.setCollisionGroup(blockCollisionGroup);
+    piece_one.body.collides([blockCollisionGroup]);
 
     cube2 = game.add.sprite(100, -100, 'cube2');
     game.physics.p2.enable(cube2);
     cube2.body.setCollisionGroup(blockCollisionGroup);
     cube2.body.collides([blockCollisionGroup]);
 
+    piece_one.body.moveDown(300);
+    piece_one.body.collideWorldBounds = true;
+    piece_one.body.damping= 0;
 
-    cube.body.moveDown(300);
-    cube.body.collideWorldBounds = true;
-    cube.body.damping= 0;
-
-    cube.body.mass= 0.1;
+    piece_one.body.mass= 0.1;
 
     cube2.body.collideWorldBounds = false;
     cube2.body.damping= 0;
@@ -53,13 +54,11 @@ function create() {
     cube2.body.mass= 0.1;
 
     cube2.body.moveDown(300);
-    // cube2.body.collides(cubeGroup);
-
-    console.log(cube2.body.collidesWith)
 }
 
 function update() {
-    if (cube.y > window.innerHeight- 200) {
+    console.log(cube2.body.collidesWith)
+    if (piece_one.y > window.innerHeight- 200) {
         // cube.body.setZeroForce();
         // cube.body.setZeroVelocity();
         // cube.body.velocity.y = 0;
