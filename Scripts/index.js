b48932dded11b639b803a5816a0bc08ff42fe479
kenny herillard <Herillard>
2019-03-19 09:27:20 +0100

    Resolve some bugs, commit before merging

--- Scripts/index.js
+++ Scripts/index.js
@@ -12,7 +12,7 @@ const game = new Phaser.Game(
 );
 
 let piece_one;
-let cube2
+let piece_two;
 
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -20,7 +20,7 @@ function preload() {
     // game.scale.pageAlignVertically = true;
     // game.stage.backgroundColor = '#eee';
     game.load.image('piece_one', '../Assets/Images/piece_one.png');
-    game.load.image('cube2', '../Assets/Images/cube.png');
+    game.load.image('piece_two', '../Assets/Images/piece_two.png');
 
     game.load.physics('physicsData', 'Assets/physics.json');
 }
@@ -29,35 +29,30 @@ function create() {
     game.physics.startSystem(Phaser.Physics.P2JS);
     let blockCollisionGroup = game.physics.p2.createCollisionGroup();
     game.physics.p2.updateBoundsCollisionGroup();
-
+    
     piece_one = game.add.sprite(100, 100, 'piece_one');
     game.physics.p2.enable(piece_one);
     piece_one.body.clearShapes();
     piece_one.body.loadPolygon('physicsData', 'piece_one');
     piece_one.body.setCollisionGroup(blockCollisionGroup);
     piece_one.body.collides([blockCollisionGroup]);
-
-    cube2 = game.add.sprite(100, -100, 'cube2');
-    game.physics.p2.enable(cube2);
-    cube2.body.setCollisionGroup(blockCollisionGroup);
-    cube2.body.collides([blockCollisionGroup]);
-
     piece_one.body.moveDown(300);
-    piece_one.body.collideWorldBounds = true;
     piece_one.body.damping= 0;
-
     piece_one.body.mass= 0.1;
 
-    cube2.body.collideWorldBounds = false;
-    cube2.body.damping= 0;
-
-    cube2.body.mass= 0.1;
-
-    cube2.body.moveDown(300);
+    piece_two = game.add.sprite(100, -100, 'piece_two');
+    game.physics.p2.enable(piece_two);
+    piece_two.body.clearShapes();
+    piece_two.body.loadPolygon('physicsData', 'piece_two');
+    piece_two.body.setCollisionGroup(blockCollisionGroup);
+    piece_two.body.collides([blockCollisionGroup]);
+    piece_two.body.damping= 0; 
+    piece_two.body.mass= 0.1;
+    piece_two.body.moveDown(300);
 }
 
 function update() {
-    console.log(cube2.body.collidesWith)
+    console.log(piece_one.body.collidesWith)
     if (piece_one.y > window.innerHeight- 200) {
         // cube.body.setZeroForce();
         // cube.body.setZeroVelocity();
